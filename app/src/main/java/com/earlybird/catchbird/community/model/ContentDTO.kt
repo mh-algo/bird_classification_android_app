8ddfeb73e5c809bfab4561e651a0186cedbac948
Somniuw <jeongwoo7757@gmail.com>
2023-04-02 16:59:19 +0900

    fix: 커뮤니티 프래그먼트 연결 및 로그인 엑티비티 연결

---
+++ app/src/main/java/com/earlybird/catchbird/community/model/ContentDTO.kt
@@ -0,0 +1,18 @@
+package com.earlybird.catchbird.community.model
+
+import java.util.HashMap
+
+data class ContentDTO(var explain: String? = null,
+                      var imageUrl: String? = null,
+                      var uid: String? = null,
+                      var userId: String? = null,
+                      var timestamp: Long? = null,
+                      var favoriteCount: Int = 0,
+                      var favorites: MutableMap<String, Boolean> = HashMap()) {
+
+    data class Comment(var uid: String? = null,
+                       var userId: String? = null,
+                       var comment: String? = null,
+                       var timestamp: Long? = null)
+}
+
