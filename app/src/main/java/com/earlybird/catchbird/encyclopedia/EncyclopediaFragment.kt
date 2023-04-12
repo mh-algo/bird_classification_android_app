601bfea073717015263fb31960fc0dcecf1cd918
minyeong <106754293+miinyeong@users.noreply.github.com>
2023-04-12 20:24:07 +0900

    feat: 도감 스피너기능 구현
    도감페이지 도감 등록된 사진 전체 사진 구별

--- app/src/main/java/com/earlybird/catchbird/encyclopedia/EncyclopediaFragment.kt
+++ app/src/main/java/com/earlybird/catchbird/encyclopedia/EncyclopediaFragment.kt
@@ -2,6 +2,7 @@ package com.earlybird.catchbird.encyclopedia
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -23,11 +24,28 @@ class EncyclopediaFragment : Fragment() {
     private val binding: FragmentEncyclopediaBinding by lazy {
         FragmentEncyclopediaBinding.inflate(layoutInflater)
     }
+    var num = 1
+    val regist = arrayListOf<BirdImageData>()
+    val data = BirdImageList.data
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
+
+        fun BirdDataList(){
+            binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+            binding.recyclerView.adapter = MyAdapter(data)
+            num = 1
+        }
+        fun BirdRegistDataList(){
+            // firebase에 있는 도감등록 새 이름과 BirdImageList.data의 새 이름과 비교하여 일치하는 새 들만 regist 배열에 추가
+            binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+            binding.recyclerView.adapter = MyAdapter(regist)
+            num = 2
+            Log.d("my", "유저 도감등록된 새 리스트 출력함수")
+
+        }
 
         var sData = resources.getStringArray(R.array.sort)
         var adapter = ArrayAdapter(requireContext(),android.R.layout.simple_list_item_1,sData)
@@ -37,6 +55,10 @@ class EncyclopediaFragment : Fragment() {
             AdapterView.OnItemSelectedListener{
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 // 전체사진, 도감등록사진 중 선택했을 때 어떤 코드 실행될지
+                when(p2){
+                    0 -> BirdDataList() // 전체사진
+                    1 -> BirdRegistDataList() // firebase와 연동해서 유저 도감등록된 새 리스트만 출력
+                }
             }
 
             override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -48,7 +70,6 @@ class EncyclopediaFragment : Fragment() {
             val intent = Intent(context, EncyclopediaRankingActivity::class.java)
             startActivity(intent)
         }
-        val data = BirdImageList.data
         binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
         binding.recyclerView.adapter = MyAdapter(data)
 
@@ -58,15 +79,37 @@ class EncyclopediaFragment : Fragment() {
             override fun onQueryTextSubmit(query: String?): Boolean {
 
                 val search = arrayListOf<BirdImageData>()
-                if(query != null) {
-                    for (i in data) {
-                        val isExist = i.birdKor!!.contains(query, ignoreCase = true)
-                        if(isExist){
-                            search.add(i)
-                        }
+                if(num == 1) {  // 전체사진 일 때
+                    if (query.equals("")) {
+                        binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+                        binding.recyclerView.adapter = MyAdapter(data)
                     }
-                    binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
-                    binding.recyclerView.adapter = MyAdapter(search)
+                    if (query != null) {
+                        for (i in data) {
+                            val isExist = i.birdKor!!.contains(query, ignoreCase = true)
+                            if (isExist) {
+                                search.add(i)
+                            }
+                        }
+                        binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+                        binding.recyclerView.adapter = MyAdapter(search)
+                    }
+                }
+                if(num == 2){   // 도감등록된 사진 버튼 눌렀을 때
+                    if (query.equals("")) {
+                        binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+                        binding.recyclerView.adapter = MyAdapter(regist)
+                    }
+                    if (query != null) {
+                        for (i in regist) {
+                            val isExist = i.birdKor!!.contains(query, ignoreCase = true)
+                            if (isExist) {
+                                search.add(i)
+                            }
+                        }
+                        binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+                        binding.recyclerView.adapter = MyAdapter(search)
+                    }
                 }
 
                 return true
@@ -75,12 +118,13 @@ class EncyclopediaFragment : Fragment() {
             override fun onQueryTextChange(newText: String?): Boolean {
 
                 // 검색창에서 글자가 변경이 일어날 때마다 호출
-                if(newText == ""){
-                    binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
-                    binding.recyclerView.adapter = MyAdapter(data)
+                // onQueryTextSubmit의  query에는 빈값이나 null을 받아들이지 않는다.
+                // 그래서 텍스트 입력 변경시 공백인 경우 다시 onQueryTextSubmit 를 호출하면서 인자로 빈공백을 넣어준다.
+                if(newText.equals("")){
+                    this.onQueryTextSubmit("");
                 }
 
-                return true
+                return false
             }
         })
         return binding.root
@@ -96,7 +140,8 @@ class EncyclopediaFragment : Fragment() {
 
             name.text = bird.birdKor
             Glide.with(view!!.context).load(bird.imageMale).centerCrop().into(image)
-
+            // if문 firebase의 유저 도감등록 정보에 있는 새 이름과 비교하여 등록되어 있는 새는
+            //android:alpha = "1"로 값 변경
 
             itemView.setOnClickListener{
                 val intent = Intent(context, EncyclopediaBirdInforActivity::class.java)
