f56cba98da80f15e0ff3dd561c308fcff13ffc96
minyeong <106754293+miinyeong@users.noreply.github.com>
2023-05-07 17:43:40 +0900

    fix: 도감등록사진 출력화면 수정

--- app/src/main/java/com/earlybird/catchbird/encyclopedia/EncyclopediaBirdRegistActivity.kt
+++ app/src/main/java/com/earlybird/catchbird/encyclopedia/EncyclopediaBirdRegistActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.Insets.add
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -11,14 +12,19 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.earlybird.catchbird.R
 import com.earlybird.catchbird.Rank
 import com.earlybird.catchbird.Regist
+import com.earlybird.catchbird.data.BirdImageData
+import com.earlybird.catchbird.data.BirdImageList
 import com.earlybird.catchbird.databinding.ActivityEncyclopediaBirdRegistBinding
 import com.earlybird.catchbird.databinding.ActivityEncyclopediaRankingBinding
 import com.earlybird.catchbird.map.MapFragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_encyclopedia_bird_infor.view.*
 import kotlinx.android.synthetic.main.item_classification.view.*
 
@@ -28,36 +34,86 @@ class EncyclopediaBirdRegistActivity : AppCompatActivity() {
         ActivityEncyclopediaBirdRegistBinding.inflate(layoutInflater)
     }
     var auth: FirebaseAuth? = null
+    var otherUid: String? = null
     var uid: String? = null
+    var birdKor: String? = null
     var firestore: FirebaseFirestore? = null
-    private var dummy = ArrayList<Regist>()
+    private var currentUserUid : String? = null
+    val data = BirdImageList.data
+    val registImageData = arrayListOf<Regist>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
         firestore = FirebaseFirestore.getInstance()
-        dummy.apply{
-            add(
-                Regist(R.drawable.dummy_bird,"2020-03-03","13/25"),
-            )
-            add(
-                Regist(R.drawable.dummy_bird,"2020-02-03","13/45"),
-            )
-            add(
-                Regist(R.drawable.dummy_bird,"2020-03-63","13/24"),
-            )
-            add(
-                Regist(R.drawable.dummy_bird,"2020-03-03","13/25"),
-            )
-            add(
-                Regist(R.drawable.dummy_bird,"2020-03-03","13/25"),
-            )
-            add(
-                Regist(R.drawable.dummy_bird,"2020-03-03","13/25"),
-            )
+        currentUserUid = auth?.currentUser?.uid
+        otherUid = intent.getStringExtra("otherUid")
+        birdKor = intent.getStringExtra("birdKor")
+        binding.encyclopediaBtnOk.setOnClickListener {
+            finish()
         }
-        binding.recyclerviewRegist.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
-        binding.recyclerviewRegist.adapter = MyAdapter(dummy)
+        binding.encyclopediaTitle.text = birdKor+" 등록 사진"
+        val db = Firebase.firestore
+        if(otherUid == "null"){
+            db.collection("birdImageData").document(currentUserUid.toString()).collection("imageInfo")
+                .get()//todo list도 만들어서 새 설명창에 버튼누르면 찍은 사진 출력되게 하기
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        if (birdKor.equals(document.data["bird"].toString())) {
+                            var image = document.data["imageUri"]
+                            registImageData.add(
+                                Regist(
+                                    image.toString(),
+                                    document.data["date"].toString(),
+                                    document.data["time"].toString()
+                                )
+                            )
+
+                        }
+                    }
+                    if(registImageData.size==0){
+                        binding.textView2.visibility = View.VISIBLE
+                        binding.recyclerviewRegist.visibility = View.INVISIBLE
+                    }else{
+                        binding.textView2.visibility = View.INVISIBLE
+                        binding.recyclerviewRegist.visibility = View.VISIBLE
+                        binding.recyclerviewRegist.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+                        binding.recyclerviewRegist.adapter = MyAdapter(registImageData)
+                    }
+
+                }
+
+        }else {
+            db.collection("birdImageData").document(otherUid.toString()).collection("imageInfo")
+                .get()//todo list도 만들어서 새 설명창에 버튼누르면 찍은 사진 출력되게 하기
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        if (birdKor.equals(document.data["bird"].toString())) {
+                            var image = document.data["imageUri"]
+                            registImageData.add(
+                                Regist(
+                                    image.toString(),
+                                    document.data["date"].toString(),
+                                    document.data["time"].toString()
+                                )
+                            )
+
+                        }
+                    }
+                    if(registImageData.size==0){
+                        binding.textView2.visibility = View.VISIBLE
+                        binding.recyclerviewRegist.visibility = View.INVISIBLE
+                    }else{
+                        binding.textView2.visibility = View.INVISIBLE
+                        binding.recyclerviewRegist.visibility = View.VISIBLE
+                        binding.recyclerviewRegist.layoutManager = LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false)
+                        binding.recyclerviewRegist.adapter = MyAdapter(registImageData)
+                    }
+                }
+
+        }
+            
     }
 
     inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view){
@@ -68,16 +124,12 @@ class EncyclopediaBirdRegistActivity : AppCompatActivity() {
 
         fun bind(regist: Regist){
             this.regist=regist
-            registImage.setBackgroundResource(R.drawable.dummy_bird)
+            Glide.with(itemView.context).load(regist.image).centerCrop().into(registImage)
             registDate.text = this.regist.date
             registTime.text = this.regist.time
 
 
-            itemView.setOnClickListener{
-                val intent = Intent(this@EncyclopediaBirdRegistActivity, MapFragment::class.java)
-
-                startActivity(intent)
-            }
+            
         }
 
     }
